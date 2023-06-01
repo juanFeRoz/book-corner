2de9a62018cea3235b474c7c6d912f6676a191dc
Juan Felipe Rojas de la Hoz <juanfeliperojas2@protonmail.com>
2023-06-01 17:01:46 -0500

    Extraje la barra de navegacion en su propio componente

---
+++ src/rpublicas/barra-navegacion.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+export function BarraNavegacion() {
+  return <Navbar bg="dark" expand="lg" variant="dark">
+    <Container fluid>
+      <Navbar.Brand href="/">Book Corner</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/inicio">Inicio</Nav.Link>
+          <Nav.Link href="/autores">Autores</Nav.Link>
+          <Nav.Link href="/generos">Generos</Nav.Link>
+          <Nav.Link href="/fechaPublica">Fecha Publicacion</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control type="search" placeholder="Buscar Libros" className="me-2" aria-label="Search" />
+          <Button variant="outline-light">Buscar</Button>
+        </Form>
+        <Nav>
+          <Nav.Link href="/signup">Sign Up</Nav.Link>
+          <Nav.Link href="/login">Login</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>;
+}
