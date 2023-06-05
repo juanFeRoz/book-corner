145fe4b45d621aa480c3c4cd174cde918ac4cb8f
Juan Felipe Rojas de la Hoz <juanfeliperojas2@protonmail.com>
2023-06-04 21:28:56 -0500

    Arregle unos errores que resultaron de eliminar login y signup, ademas recupere el sign up de Danna

--- src/SignUpModal.js
+++ src/SignUpModal.js
@@ -1,48 +1,67 @@
 import React from "react";
 import { Modal, Container, Button } from "react-bootstrap";
+import { useState } from "react";
 
 export function SignUpModal({
   show,
   handleClose
 }) {
+
+  const [password, setPassword] = useState('');
+
+  const validate = (password) => {
+    if (password.length < 5) return 'Contraseña debe ser mayor de 5 caracters';
+  }
+
+  const errorMessage = validate(password);
+
   return <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Sign Up</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Container>
-              <form action="#" className='ancho'>
+    <Modal.Header closeButton>
+      <Modal.Title>Sign Up</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Container>
+        <form action="/action_page.php" className='ancho'>
 
-                <div class="mb-3 mt-3">
-                  <label for="email" class="form-label">Email:</label>
-                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" autoComplete='off' />
-                </div>
-                <div class="mb-3">
-                  <label for="date" class="form-label">¿Cuándo naciste?</label>
-                  <input type="date" class="form-control" id="date" placeholder="Enter fecha nacimiento" name="fecha_nacimiento" />
+          <div class="mb-3 mt-3">
+            <label for="email" class="form-label" >Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" autoComplete='off' />
+          </div>
+          <div class="mb-3">
+            <label for="date" class="form-label">¿Cuándo naciste?</label>
+            <input type="date" class="form-control" id="date" placeholder="Enter fecha nacimiento" name="fecha_nacimiento" />
 
-                </div>
-                <div class="mb-3">
-                  <label for="pwd" class="form-label">Password:</label>
-                  <input type="password" class="form-control" id="pwd" placeholder="contraseña" name="password" />
-                </div>
+          </div>
+          <div class="mb-3">
+            <label for="pwd" class="form-label">Password:</label>
+            <input
+              type="password"
+              class="form-control"
+              id="pwd"
+              placeholder="contraseña"
+              name="password"
+              value={password}
+              onChange={ev => setPassword(ev.target.value)}
+            />
+            <p>{errorMessage}</p>
+
+          </div>
 
 
-                <div class="form-check mb-3">
-                  <label class="form-check-label">
-                    <input class="form-check-input" type="checkbox" name="remember" /> Acepto los terminos y condiciones
-                  </label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
-            </Container>
+          <div class="form-check mb-3">
+            <label class="form-check-label">
+              <input class="form-check-input" type="checkbox" name="remember" /> Acepto los terminos y condiciones
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </Container>
 
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cerrar
-            </Button>
-          </Modal.Footer>
-        </Modal>;
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Cerrar
+      </Button>
+    </Modal.Footer>
+  </Modal>;
 }
-  
--- src/rout/Router1.js
+++ src/rout/Router1.js
@@ -2,11 +2,8 @@ import {Routes,Route} from "react-router-dom"
 import Inicio from "../paginas/inicio"
 import Autores from "../paginas/autores"
 import Generos from "../paginas/generos"
-import Login from "../paginas/login"
-import Signup from "../paginas/Signup"
 import Error404 from "../paginas/error404"
 import Recuperacion from "../paginas/recuperacion"
-import { BarraNavegacion } from '../navbar/barra-navegacion';
 import Router2 from "./Router2"
 
 const Router1 = () => {
@@ -14,11 +11,9 @@ const Router1 = () => {
     <>
     <Routes>
         <Route path="/" element={<Inicio />} />
-        <Route path="/signup" element={<Signup/>} />
         <Route path="/inicio" element={<Inicio />} />
         <Route path="/Autores" element={<Autores />} />
         <Route path="/generos" element={<Generos />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/recuperacion" element={<Recuperacion />} />
         <Route path="/*" element={<Router2/>}/>
         <Route path="*" element={<Error404 />} />
