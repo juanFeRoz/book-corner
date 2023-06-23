ff1a89acbeeef0a2f56ab9dd27eb2c0d0601224a
valentinaoliv <danna2211334@correo.edu.co>
2023-06-22 21:47:49 -0500

    agrege la validacion del correo en el login

--- src/LoginModal.js
+++ src/LoginModal.js
@@ -1,47 +1,80 @@
 import React from "react";
 import { Modal, Container, Form, Button, InputGroup } from "react-bootstrap";
 import { useState } from "react";
-import {FaEye, FaEyeSlash} from "react-icons/fa"
- 
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 export function LoginModal({
   show,
   handleClose,
   login
 }) {
-
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
 
-  return <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Login</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <Container>
-        <Form>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
-            <Form.Label>Correo Electronico</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupPassword">
-            <Form.Label>Contraseña</Form.Label>
-            <InputGroup>
-              <Form.Control type={showPassword ? "text" : "password"} placeholder="Password" />
-              <Button onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? <FaEyeSlash /> : <FaEye/> }
-              </Button>
-            </InputGroup>
-          </Form.Group>
+  const validateEmail = () => {
+    if (!email.includes("@")) {
+      setEmailError("Correo electrónico inválido");
+      return false;
+    }
+    setEmailError("");
+    return true;
+  };
 
-          <Button type='submit' onClick={login}>
-            Login
-          </Button>
-        </Form>
-      </Container>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Cerrar
-      </Button>
-    </Modal.Footer>
-  </Modal>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateEmail()) {
+      // Realizar la lógica de inicio de sesión aquí
+      login();
+    }
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Correo Electrónico</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                isInvalid={!!emailError}
+              />
+              <Form.Control.Feedback type="invalid">
+                {emailError}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPassword">
+              <Form.Label>Contraseña</Form.Label>
+              <InputGroup>
+                <Form.Control
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </Button>
+              </InputGroup>
+            </Form.Group>
+            <Button type="submit">Login</Button>
+          </Form>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
+
