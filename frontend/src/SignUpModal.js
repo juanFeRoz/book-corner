5cebd54946bba63267f5e5d202f3788feee2d44f
Juan Felipe Rojas de la Hoz <juanfeliperojas2@protonmail.com>
2023-07-10 07:06:39 -0500

    Empezando con el signup

--- frontend/src/SignUpModal.js
+++ frontend/src/SignUpModal.js
@@ -2,66 +2,83 @@ import React from "react";
 import { Modal, Container, Button } from "react-bootstrap";
 import { useState } from "react";
 
-export function SignUpModal({
-  show,
-  handleClose
-}) {
+export function SignUpModal({ show, handleClose }) {
+    const [email, setEmail] = useState("");
 
-  const [password, setPassword] = useState('');
+    const [password, setPassword] = useState("");
 
-  const validate = (password) => {
-    if (password.length < 5) return 'Contraseña debe ser mayor de 5 caracters';
-  }
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Sign Up</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Container>
+                    <form className='ancho' onSubmit={handleClose}>
+                        <div class='mb-3 mt-3'>
+                            <label for='email' class='form-label'>
+                                Email:
+                            </label>
+                            <input
+                                type='email'
+                                class='form-control'
+                                id='email'
+                                placeholder='Enter email'
+                                name='email'
+                                autoComplete='off'
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                        </div>
+                        <div class='mb-3'>
+                            <label for='date' class='form-label'>
+                                ¿Cuándo naciste?
+                            </label>
+                            <input
+                                type='date'
+                                class='form-control'
+                                id='date'
+                                placeholder='Enter fecha nacimiento'
+                                name='fecha_nacimiento'
+                            />
+                        </div>
+                        <div class='mb-3'>
+                            <label for='pwd' class='form-label'>
+                                Password:
+                            </label>
+                            <input
+                                type='password'
+                                class='form-control'
+                                id='pwd'
+                                placeholder='contraseña'
+                                name='password'
+                                value={password}
+                                onChange={(ev) => setPassword(ev.target.value)}
+                            />
+                            <p>{errorMessage}</p>
+                        </div>
 
-  const errorMessage = validate(password);
-
-  return <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-      <Modal.Title>Sign Up</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <Container>
-        <form action="/" className='ancho'>
-
-          <div class="mb-3 mt-3">
-            <label for="email" class="form-label" >Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" autoComplete='off' />
-          </div>
-          <div class="mb-3">
-            <label for="date" class="form-label">¿Cuándo naciste?</label>
-            <input type="date" class="form-control" id="date" placeholder="Enter fecha nacimiento" name="fecha_nacimiento" />
-
-          </div>
-          <div class="mb-3">
-            <label for="pwd" class="form-label">Password:</label>
-            <input
-              type="password"
-              class="form-control"
-              id="pwd"
-              placeholder="contraseña"
-              name="password"
-              value={password}
-              onChange={ev => setPassword(ev.target.value)}
-            />
-            <p>{errorMessage}</p>
-
-          </div>
-
-
-          <div class="form-check mb-3">
-            <label class="form-check-label">
-              <input class="form-check-input" type="checkbox" name="remember" /> Acepto los terminos y condiciones
-            </label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-      </Container>
-
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={handleClose}>
-        Cerrar
-      </Button>
-    </Modal.Footer>
-  </Modal>;
+                        <div class='form-check mb-3'>
+                            <label class='form-check-label'>
+                                <input
+                                    class='form-check-input'
+                                    type='checkbox'
+                                    name='remember'
+                                />{" "}
+                                Acepto los terminos y condiciones
+                            </label>
+                        </div>
+                        <button type='submit' class='btn btn-primary'>
+                            Submit
+                        </button>
+                    </form>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant='secondary' onClick={handleClose}>
+                    Cerrar
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 }
