4e55fdcfd434b31848d1d059c12170bea8e93be3
Juan Felipe Rojas de la Hoz <juanfeliperojas2@protonmail.com>
2023-07-09 21:25:15 -0500

    Implemente el login del usuario

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,24 @@
-const userModel = require("../models/userModel");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, "xv4L4i96DNjrgq", { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
-    res.json({ msg: "login user" });
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.login(email, password);
+
+        // create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 // signup user
@@ -10,8 +26,12 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await userModel.signup(email, password);
-        res.status(200).json({ email, user });
+        const user = await User.signup(email, password);
+
+        // create a token
+        const token = createToken(user._id);
+
+        res.status(200).json({ email, token });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/authContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthContextProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthContextProvider>
+    </React.StrictMode>
 );
